2abcd907bc28e17b3d1130fa8c4ed3284534892e
Marvy <marvy@cipherprotocol.dev>
2026-07-26 15:39:01 +0100

    Add USDC contract ABI for token interactions

---
+++ integrations/contracts.py
@@ -0,0 +1,19 @@
+USDC_ABI = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function"
+    },
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "_to", "type": "address"},
+            {"name": "_value", "type": "uint256"}
+        ],
+        "name": "transfer",
+        "outputs": [{"name": "", "type": "bool"}],
+        "type": "function"
+    }
+]
